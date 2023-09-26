4136594a1d170bcf626345eb3ea1b12406c60c9c
uytyks <uytyks@udel.edu>
2023-09-26 12:08:37 -0400

    what the helllll

--- src/script.js
+++ src/script.js
@@ -6,7 +6,6 @@ function isEmail(email) {
     let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{2,}[a-zA-Z0-9])?)*$/;
     return regex.test(String(email).toLowerCase());
    }
-
 function isPhoneNum(num) {
     let regex = /^\d{3}-\d{3}-\d{4}$/;
     return regex.test(String(num).toLowerCase());
