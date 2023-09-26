82c84d877be3502914f9bec472febade7f899a04
uytyks <uytyks@udel.edu>
2023-09-26 11:56:20 -0400

    Basic Validation Funcs

--- src/script.js
+++ src/script.js
@@ -1,1 +1,16 @@
-x = 5;
+function notNull(value){
+    if(value == null) return false;
+    return (value.length > 0);
+}
+function isEmail(email) {
+    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{2,}[a-zA-Z0-9])?)*$/;
+    return regex.test(String(email).toLowerCase());
+   }
+
+function isPhoneNum(num) {
+    let regex = /^\d{3}-\d{3}-\d{4}$/;
+    return regex.test(String(num).toLowerCase());
+}
+function validate(){
+    return false;
+}
